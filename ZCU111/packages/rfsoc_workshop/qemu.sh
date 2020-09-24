42ccd29359e7257a480a9dd098b9c836bc97a150
Rock Qu <yunq@xilinx.com>
2020-09-24 14:49:45 -0700

    rely on boot.py to configure clk

--- ZCU111/packages/rfsoc_workshop/qemu.sh
+++ ZCU111/packages/rfsoc_workshop/qemu.sh
@@ -17,21 +17,6 @@ patch --ignore-whitespace /usr/local/lib/python3.6/dist-packages/pynq/lib/pynqmi
 +    #display_javascript(js, raw=True)
 EOF
 
-# Patch to have RF clock init on boot
-patch --ignore-whitespace /usr/local/bin/start_pl_server.py  << 'EOF'
---- /usr/local/bin/start_pl_server.py	2020-09-23 17:49:12.133483000 +0100
-+++ start_pl_server.py	2020-09-23 17:49:12.133483000 +0100
-@@ -3,6 +3,8 @@
- import re
- import sys
- from pynq.pl_server.server import _start_server
-+import xrfclk
- if __name__ == '__main__':
-+    xrfclk.set_all_ref_clks(409.6)
-     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-     sys.exit(_start_server())
-EOF
-
 # Install RFSoC overlays
 pip3 install https://github.com/Xilinx/SDFEC-PYNQ/releases/download/v1.0_$BOARD/rfsoc_sdfec-1.0-py3-none-any.whl
 pip3 install git+https://github.com/strath-sdr/rfsoc_qpsk@913169d --no-deps
--- ZCU111/packages/xrfclk/pre.sh
+++ ZCU111/packages/xrfclk/pre.sh
@@ -7,3 +7,4 @@ target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp -r $script_dir/pkg $target/root/xrfclk_build
+sudo cat $script_dir/boot.py >> $target/boot/boot.py
