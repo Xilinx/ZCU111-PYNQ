808d22a5126f0c8f806bdb4841df0e542bb135ba
Rock Qu <yunq@xilinx.com>
2020-09-25 10:33:34 -0700

    fix permission issue

--- ZCU111/packages/xrfclk/pre.sh
+++ ZCU111/packages/xrfclk/pre.sh
@@ -7,4 +7,4 @@ target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp -r $script_dir/pkg $target/root/xrfclk_build
-sudo cat $script_dir/boot.py >> $target/boot/boot.py
+cat $script_dir/boot.py | sudo tee -a $target/boot/boot.py
