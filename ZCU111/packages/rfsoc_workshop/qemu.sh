f18bbfd2d39356cb7ac2fb0757ec476652e7db51
Lewis Brown <lewisb@xilinx.com>
2021-10-04 16:11:34 +0100

    Update qemu.sh
    Added pynq-venv source.

--- ZCU111/packages/rfsoc_workshop/qemu.sh
+++ ZCU111/packages/rfsoc_workshop/qemu.sh
@@ -6,16 +6,7 @@ set -e
 export BOARD=ZCU111
 export PYNQ_JUPYTER_NOTEBOOKS=/home/xilinx/jupyter_notebooks
 
-# Patch out cause of "require js" error in lab
-patch --ignore-whitespace /usr/local/lib/python3.6/dist-packages/pynq/lib/pynqmicroblaze/magic.py  << 'EOF'
---- /usr/local/lib/python3.6/dist-packages/pynq/lib/pynqmicroblaze/magic.py	2019-02-21 20:26:36.000000000 +0000
-+++ magic.py	2019-04-08 22:45:31.000000000 +0000
-@@ -98,4 +98,4 @@
- if instance:
-     get_ipython().register_magics(MicroblazeMagics)
--    display_javascript(js, raw=True)
-+    #display_javascript(js, raw=True)
-EOF
+source /usr/local/share/pynq-venv/bin/activate
 
 # Install RFSoC overlays
 pip3 install https://github.com/Xilinx/SDFEC-PYNQ/releases/download/v2.0_$BOARD/rfsoc_sdfec-2.0-py3-none-any.whl --no-deps
